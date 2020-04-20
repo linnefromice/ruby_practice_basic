8521aee6e6f17cfb7ea4c1ad7b13f97f5c145adc
linnefromice <linnefromice@gmail.com>
2020-04-20 15:54:13 +0900

    take lesson 3

--- study_01/main.rb
+++ study_01/main.rb
@@ -1,5 +1,19 @@
 # 02
+# print
+=begin
 print "hello world";
 puts "hello world";
 p "hello world";
+=end
 
+# 03
+# variable
+content = "use variable"
+puts content
+content = "use variable 2"
+puts content
+
+TITLE = "TRAINING MESSAGE"
+puts TITLE
+TITLE = "TRAINING MESSAGE 2" # NG - display warning
+puts TITLE
