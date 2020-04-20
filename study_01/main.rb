9a9dc35809413e379e444b2b82d98843d1ba0007
linnefromice <linnefromice@gmail.com>
2020-04-20 16:00:25 +0900

    take lesson 3

--- study_01/main.rb
+++ study_01/main.rb
@@ -1,4 +1,4 @@
-# 02
+# 01
 # print
 =begin
 print "hello world";
@@ -6,8 +6,9 @@ puts "hello world";
 p "hello world";
 =end
 
-# 03
+# 02
 # variable
+=begin
 content = "use variable"
 puts content
 content = "use variable 2"
@@ -17,3 +18,16 @@ TITLE = "TRAINING MESSAGE"
 puts TITLE
 TITLE = "TRAINING MESSAGE 2" # NG - display warning
 puts TITLE
+=end
+
+# 03
+# object
+
+games = {
+    nintendo: "Switch",
+    sony: "PS4"
+}
+puts games[:nintendo]
+puts games
+games[:nintendo] = "3DS"
+puts games
