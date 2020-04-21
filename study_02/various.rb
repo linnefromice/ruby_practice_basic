fc9f1b5148aa62809887c91fbbe923997a873ce4
linnefromice <linnefromice@gmail.com>
2020-04-21 15:15:50 +0900

    take lesson 7 of tutorial 2

--- study_02/various.rb
+++ study_02/various.rb
@@ -14,3 +14,27 @@ module MODULE_IF
 end
 
 MODULE_IF.main
+
+# case
+
+module MODULE_CASE
+    def self.main
+        num = 3
+        case num
+            when 0
+                puts "num is ZERO"
+            when 1
+                puts "num is ONE"
+            when 2
+                puts "num is TWO"
+            when 3
+                puts "num is THREE"
+            when 4
+                puts "num is FOUR"
+            else
+                puts "num is #{num}"
+        end
+    end
+end
+
+MODULE_CASE.main
