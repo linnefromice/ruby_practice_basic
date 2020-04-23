7a8cde05be52d778edfb64b0e9df28dd6fbb1901
linnefromice <linnefromice@gmail.com>
2020-04-23 21:58:45 +0900

    take rails lecture - add delete function (add view & modify controller)

--- study_rails_01/app_second/app/controllers/posts_controller.rb
+++ study_rails_01/app_second/app/controllers/posts_controller.rb
@@ -45,6 +45,12 @@ class PostsController < ApplicationController
         end
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+
     private
         def post_params
             params[:post].permit(:title, :body)
