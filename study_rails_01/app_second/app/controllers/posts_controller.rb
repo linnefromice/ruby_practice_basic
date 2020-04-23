edfcc6f464a1446feaf1a144afe0a2f35e32c6e8
linnefromice <linnefromice@gmail.com>
2020-04-23 21:19:43 +0900

    take rails lecture - add create post function

--- study_rails_01/app_second/app/controllers/posts_controller.rb
+++ study_rails_01/app_second/app/controllers/posts_controller.rb
@@ -16,5 +16,18 @@ class PostsController < ApplicationController
     end
 
     def create
+        # debug
+        # render plain: params[:post].inspect
+        # save
+        # @post = Post.new(params[:post])
+        @post = Post.new(post_params)
+        @post.save
+        # redirect
+        redirect_to posts_path
     end
+
+    private
+        def post_params
+            params[:post].permit(:title, :body)
+        end
 end
