52d6eb174d8a4a65bbc434c624f0f23d80719aec
linnefromice <linnefromice@gmail.com>
2020-04-23 21:49:33 +0900

    take rails lecture - modify edit post function

--- study_rails_01/app_second/app/controllers/posts_controller.rb
+++ study_rails_01/app_second/app/controllers/posts_controller.rb
@@ -36,6 +36,15 @@ class PostsController < ApplicationController
         puts "[INFO] Post = #{@post.to_s}"
     end
 
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to posts_path
+        else
+            render 'edit'
+        end
+    end
+
     private
         def post_params
             params[:post].permit(:title, :body)
