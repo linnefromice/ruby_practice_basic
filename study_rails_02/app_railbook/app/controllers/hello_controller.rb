ede9f0c1be5677587bc12adfe12674e2c889ae09
linnefromice <linnefromice@gmail.com>
2020-04-27 22:13:07 +0900

    tutorial on book - add list page (display book list)

--- study_rails_02/app_railbook/app/controllers/hello_controller.rb
+++ study_rails_02/app_railbook/app/controllers/hello_controller.rb
@@ -6,4 +6,8 @@ class HelloController < ApplicationController
   def view
     @msg = "Hello, hello/view"
   end
+
+  def list
+    @books = Book.all
+  end
 end
--- study_rails_02/app_railbook/config/routes.rb
+++ study_rails_02/app_railbook/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'hello/index'
   get 'hello/view'
+  get 'hello/list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
