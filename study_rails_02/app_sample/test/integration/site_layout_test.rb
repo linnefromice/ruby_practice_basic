3095cdfe2619cea6cab3c1a1267a782de6efd7fa
linnefromice <linnefromice@gmail.com>
2020-05-05 01:59:52 +0900

    modify integration test & include applicationHelper in test

--- study_rails_02/app_sample/test/integration/site_layout_test.rb
+++ study_rails_02/app_sample/test/integration/site_layout_test.rb
@@ -1,12 +1,19 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  test "layout home" do
     get root_path
     assert_template 'static_pages/home'
+    assert_select "title", full_title
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "layout help" do
+    get help_path
+    assert_template 'static_pages/help'
+    assert_select "title", full_title("Help")
   end
 end
