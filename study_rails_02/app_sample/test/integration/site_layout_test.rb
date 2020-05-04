3a588683a8a96b3d42b4ea035b8772b195983e0e
linnefromice <linnefromice@gmail.com>
2020-05-05 02:22:16 +0900

    rails tutorial Chapter5 on website - add test cases

--- study_rails_02/app_sample/test/integration/site_layout_test.rb
+++ study_rails_02/app_sample/test/integration/site_layout_test.rb
@@ -9,6 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
   end
 
   test "layout help" do
@@ -28,4 +29,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/contact'
     assert_select "title", full_title("Contact")
   end
+
+  test "layout signup" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")
+  end
+
 end
