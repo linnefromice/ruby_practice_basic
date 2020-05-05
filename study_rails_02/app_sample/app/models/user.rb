d20b024a0c22d8af0e59b0e78010e179f9e734c2
linnefromice <linnefromice@gmail.com>
2020-05-05 10:52:39 +0900

    rails tutorial Chapter6 on website - add validation (length, presence) for password & add test

--- study_rails_02/app_sample/app/models/user.rb
+++ study_rails_02/app_sample/app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
