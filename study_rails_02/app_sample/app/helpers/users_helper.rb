eee997d5ef1d08fb3f40f0765de89d3a8468a2f9
linnefromice <linnefromice@gmail.com>
2020-05-05 12:53:50 +0900

    rails tutorial Chapter7 on website - modify show page (add function for getting avatar & modify layout)

--- study_rails_02/app_sample/app/helpers/users_helper.rb
+++ study_rails_02/app_sample/app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+    def gravatar_for(user)
+        user_id = user.id
+        gravator_base_url = "https://randomuser.me/api/portraits/thumb/men/"
+        image_tag("#{gravator_base_url}/#{user_id}.jpg", alt: user.name, class: "gravatar")
+
+        # tutorial original
+        # gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        # image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
