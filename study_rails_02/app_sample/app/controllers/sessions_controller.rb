e39127e037b63ea76a2a464b5b6a19cd58b5564d
linnefromice <linnefromice@gmail.com>
2020-05-21 14:13:11 +0900

    rails tutorial Chapter8 on website - modify login form (add error message(exist bug))

--- study_rails_02/app_sample/app/controllers/sessions_controller.rb
+++ study_rails_02/app_sample/app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # TODO
+    else
+      flash[:danger] = 'Invalid email/password conmbination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
