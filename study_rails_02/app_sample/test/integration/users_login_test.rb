69ce7f86dcd0d73a2bf31793b7326aaeb77f1208
linnefromice <linnefromice@gmail.com>
2020-05-21 17:11:33 +0900

    rails tutorial Chapter8 on website - add logout method & modify test (add logout part to login test)

--- study_rails_02/app_sample/test/integration/users_login_test.rb
+++ study_rails_02/app_sample/test/integration/users_login_test.rb
@@ -15,15 +15,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
+    # log_in
     get login_path
+    # confirm log_in
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path, count: 1
     assert_select "a[href=?]", user_path(@user), count: 1
+    # logout
+    delete logout_path
+    # confirm log_out
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 1
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0    
   end
 end
