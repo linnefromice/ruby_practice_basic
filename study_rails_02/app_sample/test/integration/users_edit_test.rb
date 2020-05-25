6ca80ad395b5e893fb04b6dd25c5519132f0cd50
linnefromice <linnefromice@gmail.com>
2020-05-25 11:21:12 +0900

    rails tutorial Chapter10 on website - allow nil to user's password (update model & test)

--- study_rails_02/app_sample/test/integration/users_edit_test.rb
+++ study_rails_02/app_sample/test/integration/users_edit_test.rb
@@ -17,7 +17,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name: name, email: email, password: "sample", password_confirmation: "sample" } }
+    patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
