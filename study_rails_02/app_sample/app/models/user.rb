13873aeb000cba068c2ebc256cac039dd899ce61
linnefromice <linnefromice@gmail.com>
2020-05-27 11:18:19 +0900

    rails tutorial Chapter11 on website - refactor

--- study_rails_02/app_sample/app/models/user.rb
+++ study_rails_02/app_sample/app/models/user.rb
@@ -40,8 +40,7 @@ class User < ApplicationRecord
 
     # アカウントを有効にする
     def activate
-        update_attribute(:activated, true)
-        update_attribute(:activated_at, Time.zone.now)
+        update_attributes(activated: true, activated_at: Time.zone.now)
     end
 
     def send_activation_email
