7425a31ff40ae566dd8188fa5891c10f9e7cf4d5
linnefromice <linnefromice@gmail.com>
2020-05-29 01:22:12 +0900

    rails tutorial Chapter12 on website - create edit form for password reset

--- study_rails_02/app_sample/app/controllers/password_resets_controller.rb
+++ study_rails_02/app_sample/app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -7,7 +10,7 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info]  "Email sent with password reset instructions"
+      flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
       flash.now[:danger] = "Email address not found"
@@ -17,4 +20,15 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
