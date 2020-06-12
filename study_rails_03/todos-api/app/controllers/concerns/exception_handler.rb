fc6ea0fd0446b2a39a08f5c8bb0715da808c5969
linnefromice <linnefromice@gmail.com>
2020-06-12 15:17:03 +0900

    update rest api tutorial part 2 - add Json Web Token function

--- study_rails_03/todos-api/app/controllers/concerns/exception_handler.rb
+++ study_rails_03/todos-api/app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,30 @@ module ExceptionHandler
     # provides the more graceful `include` method
     extend ActiveSupport::Concern
 
-    included do
-        rescue_from ActiveRecord::RecordNotFound do |e|
-            json_response({ message: e.message }, :not_found)
-        end
+    # Define custom error subclasses - rescue catches `StandardErrors`
+    class AuthenticationError < StandardError; end
+    class MissingToken < StandardError; end
+    class InvalidToken < StandardError; end
 
-        rescue_from ActiveRecord::RecordInvalid do |e|
-            json_response({ message: e.message }, :unprocessable_entity)
-        end
+    included do
+        # Define custom handlers
+        rescue_from ActiveRecord::RecordInvalid, with :four_twenty_two
+        rescue_from ActiveRecord::RecordNotFound, with :not_found_request
+        rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+        rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+        rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    end
+
+    private
+    def four_twenty_two(e)
+        json_response({ message: e.message }, :unprocessable_entity)
+    end
+
+    def unauthorized_request(e)
+        json_response({ message: e.message }, :unauthorized)
+    end
+
+    def not_found_request(e)
+        json_response( {message: e.message}, :not_found)
     end
 end
