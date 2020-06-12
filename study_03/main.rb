dc7cd45fdde60c093a5e943f02661bc0a32d483d
linnefromice <linnefromice@gmail.com>
2020-06-13 02:25:25 +0900

    add practice file - finished procedure

--- study_03/main.rb
+++ study_03/main.rb
@@ -39,3 +39,45 @@ post.block_inspect do | attribute, value |
     puts "#{attribute} = #{value}"
 end
 post.block_inspect
+
+puts "# Procedure"
+
+def show_class(&block)
+    puts "The block class is #{block.class}" if block_given?
+    yield if block_given?
+end
+show_class do
+    puts "H1! from inside the block"
+end
+
+class PostTwo
+    attr_accessor :title, :content, :author, :publish_date
+
+    def initialize(title, content, author, publish_date)
+        @title = title
+        @content = content
+        @author = author
+        @publish_date = publish_date
+    end
+
+    def block_inspect(&block)
+        self.instance_variables.each do |instance_variable|
+            stringified_instance_varible_name = instance_variable.to_s.sub('@', '')
+            # yield(stringified_instance_varible_name, self.instance_variable_get(instance_variable)) if block_given?
+            block.call(stringified_instance_varible_name, self.instance_variable_get(instance_variable)) if block_given?
+        end
+    end
+
+    def proc_inspect(block)
+        self.instance_variables.each do |instance_variable|
+            stringified_instance_varible_name = instance_variable.to_s.sub('@', '')
+            block.call(stringified_instance_varible_name, self.instance_variable_get(instance_variable))
+        end
+    end
+end
+
+proc_inspect = Proc.new do |attribute, value|
+    puts "#{attribute} = #{value}"
+end
+post_two = PostTwo.new("Title", "Content", "Author", "Publish Date")
+post_two.proc_inspect(proc_inspect)
