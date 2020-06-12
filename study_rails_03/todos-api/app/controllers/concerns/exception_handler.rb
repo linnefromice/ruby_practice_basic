48f61634719bfbd800d21c5ce50b1000d152851d
linnefromice <linnefromice@gmail.com>
2020-06-12 16:31:51 +0900

    update rest api tutorial part 2 - add authorization for api request (exist error in test)

--- study_rails_03/todos-api/app/controllers/concerns/exception_handler.rb
+++ study_rails_03/todos-api/app/controllers/concerns/exception_handler.rb
@@ -9,11 +9,14 @@ module ExceptionHandler
 
     included do
         # Define custom handlers
-        rescue_from ActiveRecord::RecordInvalid, with :four_twenty_two
-        rescue_from ActiveRecord::RecordNotFound, with :not_found_request
+        rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
         rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
         rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
         rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
+        rescue_from ActiveRecord::RecordNotFound do |e|
+            json_response({ message: e.message }, :not_found)
+        end
     end
 
     private
