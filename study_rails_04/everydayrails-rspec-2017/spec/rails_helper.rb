d6959a211cd51995c6c691d45be67fb4e81a3bce
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-07-13 01:24:05 +0900

    update everydayrails-rspec pj - finished chapter7

--- study_rails_04/everydayrails-rspec-2017/spec/rails_helper.rb
+++ study_rails_04/everydayrails-rspec-2017/spec/rails_helper.rb
@@ -56,6 +56,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  # コントローラスペックで
+  # Deviseのヘルパーメソッドをテスト内で使用する
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, type: :request
 end
