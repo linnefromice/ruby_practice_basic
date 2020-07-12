ab9b1718a24ed1fd1c1fa61e1052158aaecf147c
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-07-12 19:41:29 +0900

    update everydayrails-rspec pj - refactor before chapter5

--- study_rails_04/everydayrails-rspec-2017/spec/models/project_spec.rb
+++ study_rails_04/everydayrails-rspec-2017/spec/models/project_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
-    user = User.create(
-        first_name: "Joe",
-        last_name: "Tester",
-        email: "joetester@example.com",
-        password: "dottle-nouveau-pavilion-tights-furze"
-    )
+    user = FactoryBot.create(:user)
     user.projects.create(
         name: "Test Project",
     )
@@ -21,27 +16,14 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-    user = User.create(
-        first_name: "Joe",
-        last_name: "Tester",
-        email: "joetester@example.com",
-        password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    user = FactoryBot.create(:user)
     user.projects.create(
         name: "Test Project"
     )
-
-    other_user = User.create(
-        first_name: "Jane",
-        last_name: "Tester",
-        email: "janetester@example.com",
-        password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
+    other_user = FactoryBot.create(:user)
     other_project = other_user.projects.build(
         name: "Test Project"
     )
-
     expect(other_project).to be_valid
   end
 
