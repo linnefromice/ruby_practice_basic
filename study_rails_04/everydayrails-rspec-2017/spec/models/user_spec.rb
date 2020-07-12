9997f51abe582de31e132279a1fdd97b0b6325c7
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-07-12 18:04:21 +0900

    update everydayrails-rspec pj - finished chapter3

---
+++ study_rails_04/everydayrails-rspec-2017/spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+        first_name: "Aaron",
+        last_name: "Sumner",
+        email: "tester@example.com",
+        password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
+  it "is invalid without a first name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+  it "is invalid without a last name" do
+    User.create(
+        first_name: "Joe",
+        last_name: "Tester",
+        email: "tester@example.com",
+        password: "dottle-nouveau-paviion-tights-furze",
+    )
+    user = User.new(
+        first_name: "Jane",
+        last_name: "Tester",
+        email: "tester@example.com",
+        password: "dottle-nouveau-pavilion-tights-furze"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
+  it "returns a user's full name as a string" do
+    user = User.new(
+        first_name: "Jone",
+        last_name: "Doe",
+        email: "johndoe@exsmple.com"
+    )
+    expect(user.name).to eq "Jone Doe"
+  end
+end
