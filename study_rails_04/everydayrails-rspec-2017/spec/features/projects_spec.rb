6a55cb3feb28d2cd225cb09ec0fa6b6790b5cad9
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-07-15 01:01:49 +0900

    update everydayrails-rspec pj - proceed chapter11 (finished tdd(add project complete button))

--- study_rails_04/everydayrails-rspec-2017/spec/features/projects_spec.rb
+++ study_rails_04/everydayrails-rspec-2017/spec/features/projects_spec.rb
@@ -13,7 +13,6 @@ RSpec.feature "Projects", type: :feature do
       fill_in "Name", with: "Test Project"
       fill_in "Description", with: "Trying out Capybara"
       click_button "Create Project"
-
     }.to change(user.projects, :count).by(1)
 
     aggregate_failures do
@@ -21,5 +20,24 @@ RSpec.feature "Projects", type: :feature do
       expect(page).to have_content "Test Project"
       expect(page).to have_content "Owner: #{user.name}"
     end
+  end
+
+  # ユーザーはプロジェクトを完了済みにする
+  scenario "user completes a project" do
+    # プロジェクトを持ったユーザーを準備する
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project, owner: user)
+    # ユーザーはログインしている
+    sign_in_as user
+    # ユーザーがプロジェクト画面を開き
+    visit project_path(project)
+    expect(page).to_not have_content "Completed"
+    # "complete"ボタンをクリックすると、
+    click_button "Complete"
+    # プロジェクトは完了済としてマークされる
+    expect(project.reload.completed?).to be true
+    expect(page).to have_content "Congratulations, this project is complete!"
+    expect(page).to have_content "Completed"
+    expect(page).to_not have_button "Complete"
   end
 end
