d37e61fa982587dd15fb64151c0db0aa6f64505f
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-08-07 00:27:59 +0900

    update react-rails project 2 - setup styled-component (trial use)

--- study_rails_09/tweet-client/src/App.tsx
+++ study_rails_09/tweet-client/src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const LeftContainer = styled.div`
+  height: 100vh;
+  width: 30vw;
+  background-color: black;
+  color: white;
+`;
+const RightContainer = styled.div`
+  height: 100vh;
+  width: 70vw;
+  background-color: white;
+  color: black;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <LeftContainer>
+        Left
+      </LeftContainer>
+      <RightContainer>
+        Right
+      </RightContainer>
+    </Wrapper>
   );
 }
 
