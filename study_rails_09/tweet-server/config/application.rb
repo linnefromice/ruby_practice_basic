5dd08f8990a1a6f9840ac286d2c1c3b901a2b53d
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-08-10 23:26:06 +0900

    update react-rails project 2 - update server setting (cors)

--- study_rails_09/tweet-server/config/application.rb
+++ study_rails_09/tweet-server/config/application.rb
@@ -33,6 +33,14 @@ module TweetServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*',
+                 :headers => :any,
+                 :methods => [:get, :post, :patch, :delete, :options]
+      end
+    end
 
     # RSpec設定 -> request_spec / model_spec のみの場合
     config.generators do |g|
