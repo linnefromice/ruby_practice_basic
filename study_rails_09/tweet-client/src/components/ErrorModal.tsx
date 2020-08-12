31bd9df9d560bdea47f51197404e7cec9de05046
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-08-13 01:54:11 +0900

    update react-rails project 2 - modify error modal

--- study_rails_09/tweet-client/src/components/ErrorModal.tsx
+++ study_rails_09/tweet-client/src/components/ErrorModal.tsx
@@ -6,16 +6,33 @@ const Wrapper = styled.div`
     height: 10vh;
     width: 40vw;
     border-radius: 1.0rem;
-    border: 0.1rem solid #ffcdd2;
-    background-color: #ffcdd2;
+    border: 0.05rem solid #CC0000;
+    background: #ffcdd2;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
 `
 
 const Message = styled.div`
+    margin: 1vh 0;
     font-size: 1.0rem;
     color: #CC0000;
+`
+
+const Button = styled.div`
+    margin: 1vh 0;
+    height: 3vh;
+    width: 7.5vw;
+    border-radius: 1.0rem;
+    border: 0.05rem solid #646464;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: #646464;
+    }
 `
 
 interface Prop {
@@ -23,11 +40,16 @@ interface Prop {
     setError: React.Dispatch<React.SetStateAction<string>>
 }
 
+
 const ErrorModal = (prop:Prop) => {
+    const resetMessage = () => {
+        prop.setError("")
+    }
+
     return (
         <Wrapper>
             <Message>{prop.error}</Message>
-
+            <Button onClick={resetMessage}>OK</Button>
         </Wrapper>
     )
 }
