a9d3787d5214d895a134dcd27fe45b4aadb67578
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-08-18 01:36:07 +0900

    update react-rails project 2 - add logged-in user information component

--- study_rails_09/tweet-client/src/layouts/Drawer.tsx
+++ study_rails_09/tweet-client/src/layouts/Drawer.tsx
@@ -2,20 +2,20 @@ import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
+import UserInterface from '../models/UserInterface';
 import { UserContext } from '../global/contexts';
 import SnsLinks from '../components/drawer/SnsLinks';
 import LinkButton from '../components/common/LinkButton';
 import DrawerButton from '../components/common/DrawerButton';
 
-interface Prop {
+interface MarginLinkButtonProp {
     label: string
     link_to: string
 }
-
 const MarginContainer = styled.div`
     margin: 0.5vh 0;
 `;
-const MarginLinkButton = (prop: Prop) => {
+const MarginLinkButton = (prop: MarginLinkButtonProp) => {
     return (
         <MarginContainer>
             <LinkButton label={prop.label} link_to={prop.link_to}/>
@@ -63,17 +63,45 @@ interface AuthenticatedMenuProp {
 }
 const AuthenticatedMenu = (prop: AuthenticatedMenuProp) => {
     return (
-        <Buttons>
-            <MarginLinkButton label="Top" link_to="/home"/>
-            <MarginLinkButton label="Watch" link_to="/tweet/list"/>
-            <MarginLinkButton label="New" link_to="/tweet/new"/>
-            <DrawerButton label="Logout" onClick={prop.onClick}/>
-        </Buttons>
+        <div>
+            <Buttons>
+                <MarginLinkButton label="Top" link_to="/home"/>
+                <MarginLinkButton label="Watch" link_to="/tweet/list"/>
+                <MarginLinkButton label="New" link_to="/tweet/new"/>
+                <DrawerButton label="Logout" onClick={prop.onClick}/>
+            </Buttons>
+        </div>
+    )
+}
+
+const UserInfoWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`;
+const NameLabel = styled.div`
+    margin 0 1vw;
+    font-size: 0.75rem;
+`;
+const NameValue = styled.div`
+    margin 0 1vw;
+    font-weight: bold;
+`;
+interface UserInfoProp {
+    name: string
+}
+const UserInfo = (prop: UserInfoProp) => {
+    return (
+        <UserInfoWrapper>
+            <NameLabel>NAME</NameLabel>
+            <NameValue>{prop.name}</NameValue>
+        </UserInfoWrapper>
     )
 }
 
 const Drawer = () => {
-    const { isLogin, setIsLogin, setUser } = useContext(UserContext);
+    const { isLogin, setIsLogin, user, setUser } = useContext(UserContext);
     let history = useHistory();
 
     const logout = () => {
@@ -83,7 +111,16 @@ const Drawer = () => {
     }
 
     const Menus = () => {
-        return isLogin ? <AuthenticatedMenu onClick={logout}/> : <UnauthenticatedMenu/>
+        return isLogin ? (
+            <div>
+                <Margin>
+                    <UserInfo name={user.name}/>
+                </Margin>
+                <Margin>
+                    <AuthenticatedMenu onClick={logout}/>
+                </Margin>
+            </div>
+        ) : (<UnauthenticatedMenu/>)
     }
 
     return (
