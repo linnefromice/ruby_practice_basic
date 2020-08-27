7b38c19f1614dabf60fce8676ac5ea2fde8f56e5
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-08-28 00:00:14 +0900

    update react-rails project 2 - add requests tests & minor collection

--- study_rails_09/tweet-server/spec/models/user_url_spec.rb
+++ study_rails_09/tweet-server/spec/models/user_url_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe UserUrl, type: :model do
     end
     context '"サイト名"が存在しないとき' do
       before { url.site_name = nil }
-      it 'should be valid' do
+      it 'should be invalid' do
         expect(url).to be_invalid
       end
     end
     context '"URL"が存在しないとき' do
       before { url.url = nil }
-      it 'should be valid' do
+      it 'should be invalid' do
         expect(url).to be_invalid
       end
     end
--- study_rails_09/tweet-server/spec/requests/users_controller_spec.rb
+++ study_rails_09/tweet-server/spec/requests/users_controller_spec.rb
@@ -35,13 +35,68 @@ RSpec.describe 'UsersController', type: :request do
   end
 
   describe 'POST /sign_up' do
-    # TODO
+    context '正しいパラメータを指定したとき' do
+      before do
+        @sign_up_params = {
+            name: "sign_up_user",
+            email: "sign_up_user@example.com",
+            password: "sign_in_user_password",
+            password_confirmation: "sign_in_user_password",
+        }
+      end
+      it 'リクエストが成功すること' do
+        post sign_up_path, params: @sign_up_params
+        expect(response.status).to eq 200
+      end
+    end
   end
 
   describe 'GET /users' do
-    it 'リクエストが成功すること' do
-      get users_path
-      expect(response.status).to eq 200
+    let(:user) { create(:user) }
+    context 'パラメータを指定しなかったとき' do
+      it 'リクエストが成功すること' do
+        get users_path
+        expect(response.status).to eq 200
+      end
+    end
+
+    context '存在するユーザのIDを指定したとき' do
+      before do
+        @params = { user_id: user.id }
+      end
+      it 'リクエストが成功すること' do
+        get users_path, params: @params
+        expect(response.status).to eq 200
+      end
+    end
+
+    context '存在しないユーザのIDを指定したとき' do
+      before do
+        @params = { user_id: 0 }
+      end
+      it 'リクエストが失敗すること' do
+        get users_path, params: @params
+        expect(response.status).to eq 400
+      end
+    end
+  end
+
+  describe 'GET /users/detail/:id' do
+    let!(:user ) { create(:user) }
+    context '存在するユーザーIDを指定したとき' do
+      let!(:user_id) { user.id }
+      it 'リクエストが成功すること' do
+        get "/users/detail/#{user_id}"
+        expect(response.status).to eq 200
+      end
+    end
+
+    context '指定したユーザーIDに紐づくユーザーが存在しないとき' do
+      let!(:user_id) { 0 }
+      it 'リクエストが失敗すること' do
+        get "/users/detail/#{user_id}"
+        expect(response.status).to eq 400
+      end
     end
   end
 end
