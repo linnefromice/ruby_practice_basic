6b7fbe4baf57d40fb8599fa0c5f367d68a556142
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-11-08 01:57:35 +0900

    add query - tweets / tweet

--- study_rails_08/rails-react-server/app/graphql/types/query_type.rb
+++ study_rails_08/rails-react-server/app/graphql/types/query_type.rb
@@ -9,5 +9,17 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :tweet, Types::TweetType, null: true, description: "指定したtweetを取得する"do
+      argument :id, Int, required: true, description: "TweetID"
+    end
+    def tweet(id:)
+      Tweet.find(id)
+    end
+
+    field :tweets, [Types::TweetType], null: true, description: "tweetを全て取得する"
+    def tweets
+      Tweet.all
+    end
   end
 end
