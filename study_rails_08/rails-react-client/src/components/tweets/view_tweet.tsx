303635ef144875b2bfc7ee0a8a46aab7335e5799
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-11-08 19:51:18 +0900

    add edit mode - use state & icon

--- study_rails_08/rails-react-client/src/components/tweets/view_tweet.tsx
+++ study_rails_08/rails-react-client/src/components/tweets/view_tweet.tsx
@@ -1,15 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import axios from 'axios'
 import dayjs from 'dayjs'
 import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import { BsPencilSquare } from "react-icons/bs"
+import { FcCancel } from "react-icons/fc";
 import TweetInterface from "../../model/tweet_interface"
 
 type TweetProps = {
     tweet: TweetInterface
 }
-
 const ViewTweet: React.FC<TweetProps> = props => {
+  const [isEditMode, setIsEditMode] = useState<Boolean>(false)
   const element = props.tweet
 
   function handleDelete(tweet_id: number) {
@@ -29,9 +32,18 @@ const ViewTweet: React.FC<TweetProps> = props => {
     <Card key={`tweet.${element.id}`} className="text-center" style={{margin: "1vh 1vw"}}>
       <Card.Header>{element.user_name}</Card.Header>
       <Card.Body>
-        <Card.Text>
-          {element.sentence}
-        </Card.Text>
+        {isEditMode
+        ? <Form>
+            <Form.Control defaultValue={element.sentence} />
+          </Form>
+        : 
+          <Card.Text>{element.sentence}</Card.Text>
+        }
+        <span className="mx-1">
+          {isEditMode
+          ? <FcCancel onClick={() => setIsEditMode(false)}/>
+          : <BsPencilSquare onClick={() => setIsEditMode(true)}/>}
+        </span>
         <Button className="mx-1" variant="primary">MODIFY</Button>
         <Button className="mx-1" variant="warning" onClick={() => handleDelete(element.id)}>DELETE</Button>
       </Card.Body>
