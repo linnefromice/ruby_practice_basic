4863e66541662311b1eb4db234e6b5be7de2661f
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-11-10 00:40:08 +0900

    modify - delete logic from view_tweet (move to parent component)

--- study_rails_08/rails-react-client/src/components/tweets/view_tweet.tsx
+++ study_rails_08/rails-react-client/src/components/tweets/view_tweet.tsx
@@ -11,38 +11,36 @@ import { requestUpdate, requestDelete } from "../../utils/apis/rest"
 
 type TweetProps = {
     tweet: TweetInterface,
-    handleUpdate: Function,
-    handleDelete: Function,
+    handleUpdate: () => void,
+    handleDelete: () => void,
     isEditMode: Boolean,
     setIsEditMode: React.Dispatch<React.SetStateAction<Boolean>>,
     updatingSentence: string,
     setUpdatingSentence: React.Dispatch<React.SetStateAction<string>>
 }
 const ViewTweet: React.FC<TweetProps> = props => {
-  const element = props.tweet
-  const [isEditMode, setIsEditMode] = useState<Boolean>(false)
-  const [updatingSentence, setUpdatingSentence] = useState<string>(element.sentence)
+  const { tweet, handleUpdate, handleDelete, isEditMode, setIsEditMode, updatingSentence, setUpdatingSentence } = props
 
   return (
-    <Card key={`tweet.${element.id}`} className="text-center" style={{margin: "1vh 1vw"}}>
-      <Card.Header>{element.user_name}</Card.Header>
+    <Card key={`tweet.${tweet.id}`} className="text-center" style={{margin: "1vh 1vw"}}>
+      <Card.Header>{tweet.user_name}</Card.Header>
       <Card.Body>
         {isEditMode
         ? <Form>
             <Form.Control defaultValue={updatingSentence} onChange={e => setUpdatingSentence(e.target.value)}/>
           </Form>
         : 
-          <Card.Text>{element.sentence}</Card.Text>
+          <Card.Text>{tweet.sentence}</Card.Text>
         }
         <span className="mx-1">
           {isEditMode
           ? <FcCancel onClick={() => setIsEditMode(false)}/>
           : <BsPencilSquare onClick={() => setIsEditMode(true)}/>}
         </span>
-        <Button className="mx-1" variant="primary" onClick={() => requestUpdate(element.id, updatingSentence)} disabled={!isEditMode}>MODIFY</Button>
-        <Button className="mx-1" variant="warning" onClick={() => requestDelete(element.id)}>DELETE</Button>
+        <Button className="mx-1" variant="primary" onClick={handleUpdate} disabled={!isEditMode}>MODIFY</Button>
+        <Button className="mx-1" variant="warning" onClick={handleDelete}>DELETE</Button>
       </Card.Body>
-      <Card.Footer className="text-muted">{dayjs(element.created_at).format('YYYY/MM/DD HH:mm:ss.SSS')}</Card.Footer>
+      <Card.Footer className="text-muted">{dayjs(tweet.created_at).format('YYYY/MM/DD HH:mm:ss.SSS')}</Card.Footer>
     </Card>
   )
 }
